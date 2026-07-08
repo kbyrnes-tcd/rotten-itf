d2fb5cfca194f480210dd680edd6fabcf3d855e8
SengulC <sengulcagdal@gmail.com>
2026-07-08 17:56:50 +0100

    Inventory item: collected item disappears from scene
    Now collecting an item means it disappears from scene
    Also added a new collectible item type: green worm, collectible_item scene also dynamically renders its Sprite2D via its Item resource

--- rotten-itf-game/scripts/inventory_system/collectible_item.gd
+++ rotten-itf-game/scripts/inventory_system/collectible_item.gd
@@ -1,13 +1,16 @@
 extends Node2D
 @onready var area_2d: Area2D = $Area2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 @export var item : InvItem
+#@export var texture : Texture2D
 var player = null;
 
-#func _ready() -> void:
-	#print("i am a collectible item~" + item.name)
+func _ready() -> void:
+	$Sprite2D.texture = item.texture
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("collect"):
 		player = body
 		player.collect(item)
+		queue_free()
