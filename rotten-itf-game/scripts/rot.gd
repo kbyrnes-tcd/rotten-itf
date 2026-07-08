ec9fbe0a7762049fcefa894442157300c272f718
SengulC <sengulcagdal@gmail.com>
2026-07-08 14:13:38 +0100

    Pixelate shader on different canvas items
    Rot sprite now has pixelation shader as well as tween fade out animation working in cohesion

--- rotten-itf-game/scripts/rot.gd
+++ rotten-itf-game/scripts/rot.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_body_entered(_body: Node2D) -> void:
-	var tween = create_tween()
-	tween.tween_property(sprite_2d, "modulate:a", 0.0, 0.5)
+	var tween = create_tween()	
 	print("entered")
-	tween.tween_callback(queue_free)	
+	tween.tween_property(sprite_2d, "modulate:a", 0.0, 0.5)
+	tween.tween_callback(queue_free)
