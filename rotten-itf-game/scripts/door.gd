6b93c3cb0212f260a3e68bc1ccff39f22310ecb8
SengulC <sengulcagdal@gmail.com>
2026-07-22 15:21:45 +0100

    Essentials scene: level progression
    connected to door.gd
    door.gd script now calls game_manager.next_script()

--- rotten-itf-game/scripts/door.gd
+++ rotten-itf-game/scripts/door.gd
@@ -1,17 +1,18 @@
 extends Area2D
 
 @export var next_scene = preload("uid://h1dqndhb88on")
+@export var game_manager : Script = preload("res://scripts/game_manager.gd")
 var near_door = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") && near_door:
 		print("opening door")
-		get_tree().change_scene_to_packed(next_scene)
+		game_manager.next_level()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		near_door = true
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	near_door = false
---
+++ rotten-itf-game/scripts/game_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+static var level_prog = ["Scene_00", "Scene_01", "Scene_02"]
+static var level_root: Node2D
+
+func _ready() -> void:
+	level_root = get_node("World/LevelRoot")
+	load_level(level_prog[1]) # AUTOLOAD SCENE_00
+
+static func unload_level():
+	if level_root.get_child_count() > 0:
+		level_root.remove_child(level_root.get_child(0))
+
+static func load_level(level_name: String):
+	unload_level()
+	var path = "res://scenes/levels/%s.tscn" % level_name
+	var scene : PackedScene = load(path)
+	var scene_node : Node = scene.instantiate()
+	if (scene):
+		level_root.add_child(scene_node)
+
+static func next_level():
+	var curr_index : int = level_prog.find(level_root.get_child(0).name)
+	var next_scene : String = str(level_prog[curr_index+1])
+	load_level(next_scene)
