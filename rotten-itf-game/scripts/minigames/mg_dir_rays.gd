cf4f26aa8232c18b4fbd1e73290d95b3336ea781
SengulC <sengulcagdal@gmail.com>
2026-07-24 17:57:35 +0100

    [Buggy] Re-factored into FSM
    - first point works fine
    - i think state traversal after^ is incorrect, rays dont get updated etc.etc...
    - tryna match minoli's code in visual feedback
    - got rid of unused signals/methods

--- rotten-itf-game/scripts/minigames/mg_dir_rays.gd
+++ rotten-itf-game/scripts/minigames/mg_dir_rays.gd
@@ -1,10 +1,10 @@
 extends RayCast2D
 
-var min_dist : int = 100
+var min_dist : int = 200
 var col_point : Vector2
 var ray_origin : Vector2
 var direction_vector : Vector2
-signal can_extend
+var opp_dir_name : String
 
 # this script will be in charge of each individual ray: up, down, left, right
 # it should check if it can extend, and extend if the player is clicking (has received signal from manager)
@@ -13,23 +13,29 @@ func _ready() -> void:
 	match name:
 		"right":
 			direction_vector = Vector2(1,0)
+			opp_dir_name = "left"
 		"left":
 			direction_vector = Vector2(-1,0)
+			opp_dir_name = "right"
 		"down":
 			direction_vector = Vector2(0,1)
+			opp_dir_name = "up"
 		"up":
 			direction_vector = Vector2(0, -1)
+			opp_dir_name = "down"
 	ray_origin = global_position
 
+func get_opp_dir_name() -> String:
+	return opp_dir_name
+
 func can_i_extend():
-	var extend : bool
+	var extend : bool = false
 	# calc. nearest col.point from this ray and if it matches min_dist
-	if ray_origin.distance_to(col_point) > min_dist:
+	if is_colliding() and ray_origin.distance_to(col_point) > min_dist:
 		extend = true
-		can_extend.emit(extend)
+	# if it doesnt have a col (that matches min_dist) then it cannot extend
 	else:
 		extend = false
-		can_extend.emit(extend)
 	return extend
 	
 func _physics_process(_delta: float) -> void:
@@ -42,8 +48,6 @@ func _physics_process(_delta: float) -> void:
 	force_raycast_update()
 	if is_colliding():
 		col_point = get_collision_point()
-	else:
-		pass
 
 func update_pos(new_pos : Vector2):
 	# update pos st. global_pos = target_pos 
--- rotten-itf-game/scripts/minigames/mg_player_rot.gd
+++ rotten-itf-game/scripts/minigames/mg_player_rot.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
+# INIT: FSM, Global vars for active ray/dir
+enum State { IDLE, GROWING, STOPPED }
+var current_state : State = State.IDLE
+var active_ray : RayCast2D
+var active_dir : Vector2
+var tip_point : Vector2
+
 @export var rays: Array[RayCast2D] = []
 var pts : PackedVector2Array = PackedVector2Array([])
-var up_extend : bool
-var down_extend : bool
-var left_extend : bool
-var right_extend : bool
-# this script is in charge of the overall game and line2d, it should check which dir the user's mousepos is registering
-# and emit the right signals/call the right methods on that directional raycast2d
 
 func _ready() -> void:
 	pts = $vine.points
+	tip_point = pts[pts.size()-1]
 
-#get direction from player - up,down,left,right
 func get_snapped_direction():
 	var mouse_pos = get_global_mouse_position()
 	var diff = mouse_pos - pts[pts.size()-1]
@@ -29,53 +30,94 @@ func get_snapped_direction():
 			dir_ray = $down
 		Vector2(0, -1):
 			dir_ray = $up
+		_:
+			dir_ray = $right
 	return [dir_vector, dir_ray]
-	# returns angle dir vector, and associated ray
 
 func update_rays(new_pos : Vector2):
-	#update all dir rays to have new pos
+	var new_pos_global = $vine.to_global(new_pos)
 	for ray in rays:
-		ray.update_pos(new_pos)
+		ray.update_pos(new_pos_global)
 
+var breathe_room = Vector2(40,20)
 
-var breathe_room = Vector2(50,50)
-func _physics_process(_delta: float) -> void:
+func process_idle():
+	#print("IDLE")
+	# IDLE: waiting for click/direction
 	var dir_data = get_snapped_direction()
-	var dir = dir_data[0]
-	var active_ray : RayCast2D = dir_data[1]
-
+	active_dir = dir_data[0]
+	active_ray = dir_data[1]
 	if active_ray == null:
 		return # no valid direction
 
-	if active_ray.can_i_extend():
-		# VISUAL
-		# i can extend?! then extend until collision point
-		var preview_point_global = active_ray.get_collision_point() - (breathe_room * dir)
+	# VISUAL UPDATE: shows until col_point
+	if active_ray.is_colliding():
+		var preview_point_global = active_ray.get_collision_point() - (breathe_room * active_dir)
 		pts[pts.size() - 1] = $vine.to_local(preview_point_global)
 		$vine.points = pts
-		if Input.is_action_pressed("click"):
-			# ACTUALLY EXTENDING
-			var col_point = active_ray.get_collision_point()
-			col_point -= breathe_room * dir 
-			pts.append($vine.to_local(col_point))
-			$vine.points = pts
-			update_rays(col_point)
-	else:
-		pts[pts.size() - 1] = pts[pts.size() - 2] # idk if this will work later...
+
+func fork_check() -> bool:
+	# check other rays for possible dir-changes
+	# IGNORE current ray and opp_dir
+	var check : bool = false
+	for ray in rays:
+		if ray == active_ray or ray.name == active_ray.get_opp_dir_name():
+			continue
+		check = check or ray.can_i_extend()
+	return check
+
+func process_growing(delta: float):
+	print("GROWING")
+	# GROWING: actively extending toward active_ray's direction, checking other 3 rays each frame from the current tip for extending,
+	# and checking active_ray itself for collision (mazobstacle)
+	
+	# reposition all rays to the curr tip_point per frame for live tip-ray detection
+	update_rays(tip_point)
+	var rot_speed = 300.0
+	
+	if !active_ray.is_colliding():
+		# nothing to grow towards
+		current_state = State.STOPPED
+		return
+	
+	elif active_ray.is_colliding():
+		var col_point : Vector2 = $vine.to_local(active_ray.get_collision_point())
+		# need to convert this to vine's local space so that it can be compared to tip_point eventually
+		col_point += breathe_room * active_dir
+
+		# check OTHER 3 rays for can_i_extend() 
+		# -> if true, stop extension here... found fork point
+		if fork_check():
+			print("FORK FOUND!")
+			current_state = State.STOPPED
+			return
+
+		tip_point = tip_point + active_dir * rot_speed * delta
+		pts[pts.size()-1] = tip_point
 		$vine.points = pts
 
-func _on_up_can_extend(extend:bool) -> bool:
-	up_extend = extend
-	return extend
+		# Then, trans. to State.STOPPED and append the final point in there
+		if tip_point.is_equal_approx(col_point):
+			current_state = State.STOPPED
+			return
 
-func _on_down_can_extend(extend:bool) -> bool:
-	down_extend = extend
-	return extend
+func process_stopped():
+	print("STOPPING")
+	# STOPPED: either hit col_point or a perpendicular ray signaled as i_can_extend
+	pts.append(tip_point) 
+	$vine.points = pts
+	update_rays(tip_point)
+	current_state = State.IDLE
 
-func _on_left_can_extend(extend:bool) -> bool:
-	left_extend = extend
-	return extend
-
-func _on_right_can_extend(extend:bool) -> bool:
-	right_extend = extend
-	return extend
+func _physics_process(delta: float) -> void:
+	#print(current_state)
+	match current_state:
+		State.IDLE:
+			process_idle()
+		State.GROWING:
+			process_growing(delta)
+		State.STOPPED:
+			process_stopped()
+			
+	if Input.is_action_just_pressed("click") && current_state == State.IDLE:
+		current_state = State.GROWING
