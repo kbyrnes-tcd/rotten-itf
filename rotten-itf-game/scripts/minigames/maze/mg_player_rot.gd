9cd9b6671df03df1494f58c44f334781f4235ec1
SengulC <98541035+SengulC@users.noreply.github.com>
2026-07-29 17:34:30 +0100

    fsm for idle and growing wip

--- rotten-itf-game/scripts/minigames/maze/mg_player_rot.gd
+++ rotten-itf-game/scripts/minigames/maze/mg_player_rot.gd
@@ -5,17 +5,19 @@ enum State { IDLE, GROWING, STOPPED }
 @export var graph: MazeData
 var current_state : State = State.IDLE
 var active_node : String
+var next_node : String
 var active_dir : String
 var tip_point : Vector2
-var preview_tip_point : Vector2  # for idle debugging
-
 var pts : PackedVector2Array = PackedVector2Array([])
+var debug_vine : PackedVector2Array = PackedVector2Array([])  # for idle debugging
+var debug_tip_point : Vector2
 
 func _ready() -> void:
 	pts = $vine.points
 	tip_point = pts[pts.size()-1]
-	var start_pos = graph.graph_data["A"]["pos"]
-	print(start_pos)
+	debug_vine = $vine_debug.points
+	debug_tip_point = debug_vine[-1]
+	active_node = "A"
 
 func get_snapped_direction():
 	var mouse_pos = get_global_mouse_position()
@@ -38,7 +40,9 @@ func get_snapped_direction():
 var breathe_room = Vector2(0,0)
 
 func process_idle():
-	# IDLE: waiting for click/direction - MOUSE CONTROL
+	# IDLE: waiting for input to set direction
+	
+	# MOUSE CONTROL
 	#var dir_data = get_snapped_direction()
 	#active_dir = dir_data[0]
 	#active_ray = dir_data[1]
@@ -57,63 +61,40 @@ func process_idle():
 		return # no valid dir detected
 	
 	# VISUAL UPDATE: shows active dir until node along that edge_dir
-	#if active_ray.is_colliding():
-		#var preview_point_global = active_ray.get_collision_point()
-		#preview_tip_point = $debug_preview.to_local(preview_point_global)
-		#$debug_preview.points[-1] = preview_tip_point
-#
-#func fork_check() -> bool:
-	## check other rays for possible dir-changes
-	#var check : bool = false
-	#for ray in rays:
-		#ray.force_raycast_update()
-		## IGNORE current ray and opp_dir
-		#if ray == active_ray or ray.name == active_ray.get_opp_dir_name():
-			#continue
-		#check = check or ray.can_i_extend()
-	#return check
-#
-#func process_growing(delta: float):
+	if graph.graph_data[active_node]["edges"].keys().has(active_dir):
+		var n_node = graph.graph_data[active_node]["edges"][active_dir]
+		debug_tip_point = graph.get_pos(n_node)
+		debug_vine[-1] = $vine_debug.to_local(debug_tip_point)
+		$vine_debug.points = debug_vine
+
+func clear_preview():
+	# reset debug view
+	debug_vine = PackedVector2Array([[0,0], [0,0]])
+	$vine_debug.points = debug_vine
+	# hide arrow_dirs
+	for dir_sprite in $dir_arrows.get_children():
+		dir_sprite.visible = false
+
+func process_growing(delta: float):
 	#print("GROWING")
-	## GROWING: actively extending toward active_ray's direction, checking other 3 rays each frame from the current tip for extending,
-	## and checking active_ray itself for collision (mazobstacle)
-	#
-	## reposition all rays to the curr tip_point per frame for live tip-ray detection
-	#update_rays(tip_point)
-	#var rot_speed = 300.0
-	##active_ray.active()
-	#
-	#if !active_ray.is_colliding():
-		## nothing to grow towards
-		#current_state = State.STOPPED
-		#return
-	#
-	#elif active_ray.is_colliding():
-		##("collision detected ahead, visual growth update incoming")
-		#var col_point : Vector2 = $vine.to_local(active_ray.get_collision_point())
-		## need to convert this to vine's local space so that it can be compared to tip_point eventually
-		#col_point += breathe_room * active_dir
-#
-		## VISUAL UPDATE: move tip along
-		#tip_point = tip_point + active_dir * rot_speed * delta
-		#pts[pts.size()-1] = tip_point
-		#$vine.points = pts
-		#
-		## check OTHER 3 rays for can_i_extend() 
-		## -> if true, stop extension here... found fork point
-		#if fork_check():
-			#print("FORK FOUND!")
-			#current_state = State.STOPPED
-			#return
-#
-		## Then, trans. to State.STOPPED and append the final point in there
-		#if tip_point.is_equal_approx(col_point):
-			#current_state = State.STOPPED
-			#return
-#
+	var grow_speed: float = 200.0  # pixels /sec
+
+	# if active_node can grow towards active_dir, do... extend the tip
+	if graph.graph_data[active_node]["edges"].keys().has(active_dir):
+		# clear debug preview & arrow_dirs
+		clear_preview()
+		next_node = graph.graph_data[active_node]["edges"][active_dir]
+		tip_point = graph.get_pos(next_node)
+
+		var current_tip = to_global(pts[-1])
+		var moving_tip = current_tip.move_toward(tip_point, grow_speed * delta)
+		pts[-1] = $vine.to_local(moving_tip)
+
+		$vine.points = pts
+	else: current_state = State.IDLE
+
 #func process_stopped():
-	#print("STOPPING")
-	## add tip_point to points, moving rot along concretely & update its rays from that pos.
+	# add tip_point to points, moving rot along concretely & update its dir_arrows to that pos.
 	#pts.append(tip_point)
 	#$vine.points = pts
 	#update_rays(tip_point)
@@ -122,8 +103,8 @@ func process_idle():
 	#var tip_global = $vine.to_global(tip_point)
 	#var tip_local_preview = $debug_preview.to_local(tip_global)
 	#$debug_preview.points = PackedVector2Array([tip_local_preview, tip_local_preview])
-#
-	#active_dir = Vector2(0,0)
+
+	#active_dir = null
 	#active_ray = null
 	#current_state = State.IDLE
 
@@ -132,8 +113,8 @@ func _physics_process(delta: float) -> void:
 	match current_state:
 		State.IDLE:
 			process_idle()
-		#State.GROWING:
-			#process_growing(delta)
+		State.GROWING:
+			process_growing(delta)
 		#State.STOPPED:
 			#process_stopped()
 			
