ddb10f39beffa7df2ca6fc45d7cc26531975559f
SengulC <98541035+SengulC@users.noreply.github.com>
2026-08-02 17:10:18 +0100

    Building layered level-block
    using canvas layers to define layers of envo. art, i.e. Background pieces have layer -1

--- rotten-itf-game/scripts/game_globals.gd
+++ rotten-itf-game/scripts/game_globals.gd
@@ -14,7 +14,7 @@ enum Minigame { NONE, MAZE, LETTER }
 static var pause_menu: Control
 static var color_rect: ColorRect
 
-var running_another_scene : bool = false # for running minigames and etc.
+var running_another_scene : bool = true # for running minigames and etc.
 
 func _ready() -> void:
 	if !running_another_scene:
