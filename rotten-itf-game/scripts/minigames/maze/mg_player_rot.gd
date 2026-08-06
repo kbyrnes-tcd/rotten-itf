0578e79b8c2543ea6363b9f27ae965ce66fcab54
SengulC <98541035+SengulC@users.noreply.github.com>
2026-08-06 14:03:25 +0100

    Pushing visual code (WIP)

--- rotten-itf-game/scripts/minigames/maze/mg_player_rot.gd
+++ rotten-itf-game/scripts/minigames/maze/mg_player_rot.gd
@@ -115,7 +115,7 @@ func process_stopped():
 	pts.append($vine.to_local(tip_point))
 	$vine.points = pts
 	print('appended point calling update...')
-	RotVisual.update_texture_indices($vine.points)
+	#RotVisual.update_texture_indices($vine.points)
 	
 	var incoming_dir = active_dir
 	active_node = next_node
