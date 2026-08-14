ce96e996d261b6221c2832b1b5090d4452bc2fc6
MinoliMathew <minolimathew@gmail.com>
2026-08-14 20:02:36 +0100

    added movement and new sprites to chandelier

--- rotten-itf-game/scripts/chandelier.gd
+++ rotten-itf-game/scripts/chandelier.gd
@@ -2,37 +2,49 @@ extends Node2D
 
 enum State { HANGING, LOWERED, FALLEN}
 var state = State.HANGING
-
+var chain_links: Array = []
 @onready var anim = $AnimationPlayer
+@onready var chain = $Chain
 
 
 func lower():
 	if state == State.HANGING:
 		state = State.LOWERED
-		# shake first then lower
 		var original_pos = position
 		var tween = create_tween()
-		# shake left and right a few times
+		_grow_chain()
+		#shake chandelier
 		tween.tween_property(self, "position:x", original_pos.x + 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x - 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x + 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x - 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x, 0.1)
-		# then play lower animation after shake finishes
-		tween.tween_callback(func(): anim.play("lower"))
-
+		tween.tween_callback(func(): 
+			anim.play("lower"))
+		
+func _grow_chain():
+	var timer = get_tree().create_timer(0.3)
+	timer.timeout.connect(func():
+		_add_chain_link()
+		if chain_links.size() < 5:
+			_grow_chain())
+func _add_chain_link():
+	var link = chain.duplicate()
+	add_child(link)
+	var link_height = chain.texture.get_height()
+	link.position = Vector2(chain.position.x, chain.position.y - (chain_links.size() + 1) * link_height * 0.55)
+	chain_links.append(link)
+	
 func fall():
 	if state == State.LOWERED:
 		state = State.FALLEN
 		var original_pos = position
 		var tween = create_tween()
-		# shake before falling
 		tween.tween_property(self, "position:x", original_pos.x + 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x - 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x + 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x - 5, 0.1)
 		tween.tween_property(self, "position:x", original_pos.x, 0.1)
-		# then fall after shake
 		tween.tween_property(self, "position:y", 2000.0, 2.5)\
 			.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN)
 		
