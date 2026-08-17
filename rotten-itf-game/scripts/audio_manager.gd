da6382a649ea99a13799f32f54d06293d785397b
SengulC <sengulcagdal@gmail.com>
2026-08-17 15:19:34 +0100

    Ambience and Music depending on scene/env

--- rotten-itf-game/scripts/audio_manager.gd
+++ rotten-itf-game/scripts/audio_manager.gd
@@ -1,27 +1,35 @@
 extends Node
 
+# audiostreamplayers
 var active_ambience : AudioStreamPlayer
+var active_music : AudioStreamPlayer
 var active_fx : AudioStreamPlayer # looping fx that only one of can play at a time e.g. grow/shrink
 var active_walk_fx : AudioStreamPlayer # walking fx should be able to be layered
 @onready var active_os: AudioStreamPlayer = $Clips/OneShotFX
+
+# fade in/out tweens
 var walk_tween : Tween
 var fx_tween : Tween
+var am_tween : Tween
+var music_tween : Tween
 
+# source fx/music files
 @export var clips: Node
 @export_group("Source")
-@export var fx: Dictionary[String, AudioStream] = {
-}
-@export_group("Arrays")
+@export var fx: Dictionary[String, AudioStream] = {}
+@export_group("Music")
+@export var ambiences : Dictionary[String, AudioStream] = {}
+@export var music : Dictionary[String, AudioStream] = {}
+@export_group("FX_Arrays")
 @export var win_fx: Array[AudioStream] = []
 @export var scraps_fx: Array[AudioStream] = []
 @export var alphabet_fx : Array[AudioStream] = []
 
-# need 2 rework like w/ play_os to play dynamic ambience
-func play_ambience(am_name: String, from: float = 0.0, skip_restart: bool = false) -> void:
-	if skip_restart and active_ambience and active_ambience.name == am_name:
-		return
-	active_ambience = clips.get_node(am_name)
-	active_ambience.play(from)
+func get_cur_song():
+	return "%s & %s" %[active_music.stream.resource_path.get_file().get_basename(), active_music.volume_db]
+
+func get_cur_ambience():
+	return "%s & %s" %[active_ambience.stream.resource_path.get_file().get_basename(), active_ambience.volume_db]
 
 func fade(c_tween:Tween, stream:AudioStreamPlayer, start_vol := 0.0, end_vol := 0.0, dur := 0.5):
 	if c_tween:
@@ -35,31 +43,76 @@ func fade(c_tween:Tween, stream:AudioStreamPlayer, start_vol := 0.0, end_vol := 
 	# if fading out
 	if end_vol < start_vol:
 		stream.stop()
+		#print("fading out: %s" % stream.stream.resource_path.get_file().get_basename())
+	#else:
+		#print("Fading in")
+	return c_tween
 
-	return
+func change_ambience(am_name: String) -> void:
+	if active_ambience:
+		var c_am := active_ambience.stream.resource_path.get_file().get_basename()
+		# if it's the same track, keep continue
+		if c_am == am_name: return
+		else: 
+			#print("not the same am so changing TO %s from %s" %[am_name, c_am])
+			am_tween = await fade(am_tween, active_ambience, -10.0, -30.0, 1.0) # _tween = await fadeout current track b4 changing
+			active_ambience = null
+			play_ambience(am_name)
+
+func play_ambience(am_name: String) -> void:
+	if !active_ambience:
+		active_ambience = clips.get_node("Ambience")
+		active_ambience.stream = ambiences[am_name]
+		#print("fading in %s" %am_name)
+		am_tween = await fade(am_tween, active_ambience, -25.0, -10.0, 1.0)
+		#print("calling play")
+		active_ambience.play()
+
+func change_music(song: String) -> void:
+	if active_music:
+		var c_song := active_music.stream.resource_path.get_file().get_basename()
+		# if it's the same track, keep continue
+		if c_song == song: return
+		else: 
+			music_tween = await fade(music_tween, active_music, -30.0, -50.0, 1.0) # fade current track b4 changing
+			active_music = null
+			play_music(song)
+
+func play_music(song: String) -> void:
+	if !active_music:
+		active_music = clips.get_node("Music")
+		active_music.stream = music[song]
+		music_tween = await fade(music_tween, active_music, -50.0, -30, 1.0)
+		active_music.play()
+
+func increase_music_vol():
+	music_tween = await fade(music_tween, active_music, active_music.volume_db, active_music.volume_db + 15.0)
+
+func decrease_music_vol():
+	music_tween = await fade(music_tween, active_music, active_music.volume_db, active_music.volume_db - 15.0)
 
 func play_walk_fx() -> void:
 	if !active_walk_fx:
 		active_walk_fx = clips.get_node("WalkFX")
 		active_walk_fx.stream = fx["walk"]
-		fade(walk_tween, active_walk_fx, -10.0, 0.0, 0.2)
+		walk_tween = await fade(walk_tween, active_walk_fx, -10.0, 0.0, 0.2)
 		active_walk_fx.play()
 
 func stop_walk_fx() -> void:
 	if active_walk_fx:
-		fade(walk_tween, active_walk_fx, 0.0, -10.0, 0.2)
+		walk_tween = await fade(walk_tween, active_walk_fx, 0.0, -10.0, 0.2)
 		active_walk_fx = null
 
 func play_fx(fx_name: String, from: float = 0.0) -> void:
 	if !active_fx:
 		active_fx = clips.get_node("LoopFX")
 		active_fx.stream = fx[fx_name]
-		fade(fx_tween, active_fx, -50.0, -15.0, 1.0)
+		fx_tween = await fade(fx_tween, active_fx, -50.0, -15.0, 1.0)
 		active_fx.play(from)
 	
 func stop_fx() -> void:
 	if active_fx:
-		fade(fx_tween, active_fx, -15.0, -50.0, 1.0)
+		fx_tween = await fade(fx_tween, active_fx, -15.0, -50.0, 1.0)
 		active_fx = null
 
 func _process(_delta: float) -> void:
@@ -83,7 +136,7 @@ func play_dialog(copy: String, base_pitch: float = 1.75, pitch_variance: float =
 func play_os_from_arr(arr_name: String, index : int = -1, from: float = 0.0) -> void:
 	var os_arr = get(arr_name + "_fx")
 	active_os.stream = os_arr[index] if index != -1 else os_arr.pick_random()
-	#print("playing os %s" %index)
+	print("playing os %s" %index)
 	active_os.play(from)
 	return
 	
--- rotten-itf-game/scripts/game_globals.gd
+++ rotten-itf-game/scripts/game_globals.gd
@@ -1,13 +1,15 @@
 extends Node
 class_name GameGlobals
 
-static var level_prog = ["Begin", "Scene_01", "Scene_02", "Scene_03"]
+static var level_prog = ["Scene_01", "Scene_02_int", "Scene_02_ext", "Scene_03", "Scene_05"]
+static var prog_counter := 0
 # PROGRESSION: ext_path, garden_int, garden_ext, altar, quarters, tower, p-altar, underworld
 static var audio_prog := [
 	{"ambience": "Ext_Ambiance","music": "Temple_Music"}, #ext_path
 	{"ambience": "Int_Ambiance","music": "Temple_Music"}, #garden_int
 	{"ambience": "Garden_Ambience", "music": "Temple_Music"}, #garden_ext
-	{"ambience": "Int_Ambiance", "music": "Temple_Music"} #altar
+	{"ambience": "Int_Ambiance", "music": "Temple_Music"}, #test
+	{"ambience": "Int_Ambiance", "music": "Temple_Music"} #test
 	#{"ambience": "", "music": ""}
 ]
 
@@ -26,11 +28,10 @@ static var letter_ui: Control
 
 static var mid_mg : Node
 static var inv_ui : Control
-var running_another_scene : bool = true # for running minigames and etc.
+var running_another_scene : bool = false # for running minigames and etc.
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	AudioManager.play_ambience("Ambience", 0, true)
 	tree = get_tree()
 	scene = tree.current_scene
 	player = scene.find_child("Player")
@@ -80,7 +81,7 @@ static func pause(w_menu : bool):
 
 static func unload_mid_minigame():
 	#print("Tryna unload mid mg")
-	# resume game
+	#AudioManager.decrease_music_vol()
 	current_mg = Minigame.NONE
 	resume(false)
 	# store mg_root child from tree into mid_mg
@@ -92,6 +93,7 @@ static func unload_mid_minigame():
 static func unload_minigame():
 	print("player has won, unloading fr")
 	AudioManager.play_os_from_arr("win")
+	#AudioManager.decrease_music_vol()
 	current_mg = Minigame.NONE
 	resume(false)
 	# unload mg_root child from tree
@@ -104,6 +106,7 @@ static func unload_minigame():
 	return
 	
 static func load_minigame(mg : int):
+	#AudioManager.increase_music_vol()
 	if inv_ui.visible or letter_ui.visible:
 		inv_ui.close()
 		letter_ui.visible = false
@@ -152,8 +155,17 @@ static func load_level(level_name: String):
 	if (c_scene):
 		level_root.add_child(scene_node)
 		player = scene_node.find_child("Player")
+		if prog_counter != 0:
+			print("Calling next audio in load_level")
+			AudioManager.change_ambience(audio_prog[prog_counter].ambience)
+			AudioManager.change_music(audio_prog[prog_counter].music)
+		else: 
+			print("Calling FIRST amb audio in load_level")
+			AudioManager.play_ambience(audio_prog[0].ambience)
+			AudioManager.play_music(audio_prog[0].music)
 
 static func next_level():
 	var curr_index : int = level_prog.find(level_root.get_child(0).name)
 	var next_scene : String = str(level_prog[curr_index+1])
+	prog_counter += 1
 	load_level(next_scene)
