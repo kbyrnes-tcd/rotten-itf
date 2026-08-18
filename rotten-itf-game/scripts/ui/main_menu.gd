38fe0c0659039ed4e8613a7b40b46d10bfd670bc
SengulC <sengulcagdal@gmail.com>
2026-08-18 17:04:06 +0100

    Main menu + GameGlobals polish
    main screen->begin-> levels progression working! start main_screen scene w/ running_another_scene = true
    
    GameGlobals was being used incorrectly basically, removed all static stuff, not a class anymore, all access is directly on the single Autoload instance.

---
+++ rotten-itf-game/scripts/ui/main_menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var main_btns = $BorderBox/VBoxContainer/MainButtons
+@onready var settings_panel = $BorderBox/VBoxContainer/SettingsPanel
+@onready var volume_slider = $BorderBox/VBoxContainer/SettingsPanel/HBoxContainer/VolumeSilder
+
+func _ready():
+	settings_panel.visible = false 
+	_setup_hovers()
+	volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+
+func _setup_hovers():
+	var buttons := get_tree().get_nodes_in_group("buttons")
+	buttons.map(func(b): _setup_hover(b))
+
+func _setup_hover(btn: Button):
+	var sprite = btn.get_node("HoverSprite")
+	sprite.visible = false
+	btn.mouse_entered.connect(func(): sprite.visible = true)
+	btn.mouse_exited.connect(func(): sprite.visible = false)
+
+func _on_begin_pressed() -> void:
+	GameGlobals.start_new_game()
+
+func _on_settings_pressed() -> void:
+	main_btns.visible = false
+	settings_panel.visible = true
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_back_pressed():
+	settings_panel.visible = false
+	main_btns.visible = true
+
+func _on_volume_silder_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
