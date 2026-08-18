04f9e2077cb6037021c6b3a5e321b41d05314791
SengulC <sengulcagdal@gmail.com>
2026-08-18 16:29:14 +0100

    Pause menu polsihed

--- rotten-itf-game/scripts/ui/pause_menu.gd
+++ rotten-itf-game/scripts/ui/pause_menu.gd
@@ -1,20 +1,22 @@
 extends Control
 
-const HOVER_SPRITE = preload("res://assets/images/ui/button_pause.png")
+#const HOVER_SPRITE = preload("res://assets/images/ui/button_pause.png")
 
-@onready var resume_btn = $BorderBox/VBoxContainer/MainButtons/Resume
-@onready var settings_btn = $BorderBox/VBoxContainer/MainButtons/Settings
-@onready var quit_btn = $BorderBox/VBoxContainer/MainButtons/Quit
+#@onready var resume_btn = $BorderBox/VBoxContainer/MainButtons/Resume
+#@onready var settings_btn = $BorderBox/VBoxContainer/MainButtons/Settings
+#@onready var quit_btn = $BorderBox/VBoxContainer/MainButtons/Quit
 @onready var main_btns = $BorderBox/VBoxContainer/MainButtons
 @onready var settings_panel = $BorderBox/VBoxContainer/SettingsPanel
 @onready var volume_slider = $BorderBox/VBoxContainer/SettingsPanel/HBoxContainer/VolumeSilder
 
 func _ready():
 	settings_panel.visible = false 
-	_setup_hover(resume_btn)
-	_setup_hover(settings_btn)
-	_setup_hover(quit_btn)
+	_setup_hovers()
 	volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(0))
+
+func _setup_hovers():
+	var buttons := get_tree().get_nodes_in_group("buttons")
+	buttons.map(func(b): _setup_hover(b))
 
 func _setup_hover(btn: Button):
 	var sprite = btn.get_node("HoverSprite")
@@ -45,22 +47,18 @@ func _on_quit_pressed() -> void:
 func _on_back_pressed():
 	settings_panel.visible = false
 	main_btns.visible = true
-			
-			
+
 func _on_stuck_pressed() -> void:
 	GameGlobals.resume(false)
 	var current_level = GameGlobals.level_root.get_child(0).name
 	GameGlobals.load_level(current_level)
 	GameGlobals.resume(true)
 
-
 func _on_refill_pressed() -> void:
 	var player = get_tree().get_first_node_in_group("player")
 	if player:
 		for i in 5:
 			player.inv.insert(player.GLOWWORM)
-		print("worms refilled")
-
 
 func _on_volume_silder_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0, linear_to_db(value))
