306460c687f8b896a9afdc252a828a990e0013ef
SengulC <sengulcagdal@gmail.com>
2026-08-19 18:27:32 +0100

    Uncommented line that was NOT buggy...
    i am juust lazy... run intro> begin pipeline

--- rotten-itf-game/scripts/rot_vine.gd
+++ rotten-itf-game/scripts/rot_vine.gd
@@ -103,8 +103,8 @@ func _on_area_2d_body_entered(_body: Node2D) -> void:
 	pass
 
 func _on_line_area_2d_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	#if GameGlobals.player.get_tool_state() == 1 and area.name == "LanternArea2D":
-	if area.name == "LanternArea2D":
+	if GameGlobals.player.get_tool_state() == 1 and area.name == "LanternArea2D":
+	#if area.name == "LanternArea2D":
 		AudioManager.play_fx("shrink")
 		var space_state = get_world_2d().direct_space_state
 		var indices = [] # to store points within lantern-area; to later split at
