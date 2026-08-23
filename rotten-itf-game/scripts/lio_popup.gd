d719e55ec5855cf5fe74898dd688b7b560bc13b6
SengulC <sengulcagdal@gmail.com>
2026-08-23 16:49:56 +0100

    LIO dark frame fadein/out too and scene1 right limit fixed

--- rotten-itf-game/scripts/lio_popup.gd
+++ rotten-itf-game/scripts/lio_popup.gd
@@ -12,31 +12,39 @@ func _ready() -> void:
 	label.custom_minimum_size = Vector2(400, 0)
 	dark_frame.visible = false
 	label.modulate.a = 0.0
-	dark_frame.modulate.a = 0.5
+	dark_frame.modulate.a = 0.0
 
-func _show_text(text: String, duration: float = 3.0, fade: float = 1.0):
+func _show_text(text: String, duration: float = 3.0, fade: float = 1.0, seq := false):
 	dark_frame.visible = true
+	var in_tween = create_tween()
+	await in_tween.tween_property(dark_frame, "modulate:a", 0.5, 0.5).finished
+	
 	if current_tween:
 		current_tween.kill()
-	
+
 	label.text = text
 	label.modulate.a = 1.0
-	
+
 	current_tween = create_tween()
 	current_tween.tween_interval(duration)
 	current_tween.tween_property(label, "modulate:a", 0.0, fade)
-	current_tween.tween_callback(func(): dark_frame.visible = false)
-		
+
+	if !seq:
+		await current_tween.finished
+		var out_tween = create_tween()
+		await out_tween.tween_property(dark_frame, "modulate:a", 0.0, 1.0).finished
+		dark_frame.visible = false
 
 func show_sequence(lines: Array, duration: float = 3.0):
 	dark_frame.visible = true
+	dark_frame.modulate.a = 0.0
+	var in_tween = create_tween()
+	await in_tween.tween_property(dark_frame, "modulate:a", 0.5, 0.5).finished
+
 	for line in lines:
-		label.text = line
-		label.modulate.a = 1.0
-		if current_tween:
-			current_tween.kill()
-		current_tween = create_tween()
-		current_tween.tween_interval(duration)
-		current_tween.tween_property(label, "modulate:a", 0.0,1.0)
+		_show_text(line, duration, 1.0, true)
 		await current_tween.finished
+
+	var out_tween = create_tween()
+	await out_tween.tween_property(dark_frame, "modulate:a", 0.0, 1.0).finished
 	dark_frame.visible = false
