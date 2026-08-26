316222cb7d827c5f05a6d22bd8a343d5889c6adb
SengulC <sengulcagdal@gmail.com>
2026-08-26 14:44:44 +0100

    interactive carat for first half-ish

--- rotten-itf-game/scripts/interactive_carat.gd
+++ rotten-itf-game/scripts/interactive_carat.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@export var unlocked_carat := false
+@export var unlocked_carat := true
 
 func _ready():
 	sprite_2d.visible = false
@@ -12,9 +12,10 @@ func hide_carat():
 	sprite_2d.visible = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body.name)
 	if body.name == "Player" and unlocked_carat:
-		sprite_2d.visible = true
+		show_carat()
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Player" and unlocked_carat:
-		sprite_2d.visible = false
+		hide_carat()
