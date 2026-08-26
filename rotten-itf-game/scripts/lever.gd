5a2b0eb64dc7a77b684338dd0313aa6272456a90
SengulC <sengulcagdal@gmail.com>
2026-08-26 15:00:25 +0100

    LEVER GO DOWNNNN

--- rotten-itf-game/scripts/lever.gd
+++ rotten-itf-game/scripts/lever.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 var player_in_area = false
 var pulled = false
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var hint = $InteractHint
 @export var chandelier: Node
-
+@export var pulled_texture : Texture2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -18,6 +18,7 @@ func _process(_delta):
 		pulled = true
 		print("lower chandelier")
 		chandelier.lower()
+		sprite_2d.texture = pulled_texture
 
 func _on_body_entered(body):
 	if body.has_method("collect"):
